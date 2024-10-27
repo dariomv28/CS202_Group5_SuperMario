a126e19f9f9882acb4523de11311061aa819f4da
KietAPCS <phantuankiet688@gmail.com>
2024-10-27 15:28:39 +0700

    Setting required sfml packages and test SFML graphics.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Mario Game");
 
-	cout << "Hello World!";
-
-
-	return 0;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    return 0;
 }
