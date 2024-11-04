ad5f766811261f40228f5e43bff906a569061bf3
KietAPCS <phantuankiet688@gmail.com>
2024-11-04 17:09:34 +0700

    Creating the class GameState inherited from the base (State Class)

---
+++ Source/GameState.cpp
@@ -0,0 +1,30 @@
+#include "Headers/GameState.h"
+
+GameState::GameState(sf::RenderWindow* window) : State(window) {
+
+}
+
+GameState::~GameState() {
+
+}
+
+void GameState::endState() {
+	std::cout << "Ending GameState!" << "\n";
+}
+
+void GameState::updateKeyBinds(const float& dt) {
+	this->checkForQuit();
+}
+
+void GameState::update(const float& dt) {
+	// Update the state of GameState
+	this->updateKeyBinds(dt);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		std::cout << "A" << "\n";
+	}
+}
+
+void GameState::render(sf::RenderTarget* target) {
+
+}
