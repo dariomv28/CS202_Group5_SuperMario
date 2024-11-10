2e40ccb17a3a1c41c29fe97216b584ea0e8bfc5e
Dang Vo <146331706+dariomv28@users.noreply.github.com>
2024-11-11 03:09:34 +0700

    modify physics

--- Source/PhysicsEngine.cpp
+++ Source/PhysicsEngine.cpp
@@ -3,4 +3,9 @@
 void PhysicsEngine::applyGravity(GameObject& obj) {
 	obj.setForce(gravity);
 }
+bool PhysicsEngine::checkCollision(GameObject& obj1, GameObject& obj2) {
+	return obj1.checkCollision(obj2);
+}
+void PhysicsEngine::resolveCollision(GameObject& obj1, GameObject& obj2) {
 
+}
