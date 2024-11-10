b57b0d4c77c8ea7ac2002f5ffd6ccdb24586b7e3
Dang Vo <146331706+dariomv28@users.noreply.github.com>
2024-11-11 00:29:50 +0700

    update GameObject

---
+++ Source/Headers/GameObject.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdafx.h"
+class GameObject
+{
+protected:
+	sf::Vector2f position;
+	sf::Vector2f velocity;
+	sf::Vector2f size;
+public:
+	GameObject() {};
+	GameObject(sf::Vector2f position, sf::Vector2f velocity, sf::Vector2f size);
+	GameObject(const GameObject& other);
+	virtual void move() = 0;
+	bool checkCollision(const GameObject& other);
+	sf::Vector2f getPosition();
+
+};
