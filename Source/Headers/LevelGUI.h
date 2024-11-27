84ecacaee1835cf978821f8fe6d9972d62372f44
Dang Vo <146331706+dariomv28@users.noreply.github.com>
2024-11-27 16:47:30 +0700

    modify levelGUI

--- Source/Headers/LevelGUI.h
+++ Source/Headers/LevelGUI.h
@@ -1,10 +1,12 @@
 #pragma once
-#include "stdafx.h"
+#include "GUI.h"
+#include "PlayerManager.h"
 class LevelGUI
 {
 private:
 	int health;
 	int coinsCollected;
-
+	sf::RectangleShape healthBar;
+	
 };
 
