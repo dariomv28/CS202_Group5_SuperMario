a154ef67ea16a70262a249af41c0a7b5d709c9eb
Dell <Dell@DESKTOP-PRIISU0>
2024-11-28 22:58:37 +0700

    Fix more collision bugs again

--- Source/PhysicsEngine.cpp
+++ Source/PhysicsEngine.cpp
@@ -3,7 +3,6 @@
 #include "Headers/Block.h"
 #include "Headers/LivingEntity.h"
 #include "Headers/Enemy.h"
-#include <iostream>
 
 PhysicsEngine::PhysicsEngine() {
 	gravity = sf::Vector2f(0, 2.0f * PIXELS_PER_METER);
@@ -40,16 +39,24 @@ void PhysicsEngine::applyFriction(LivingEntity* entity, const float& dt) {
 }
 
 bool PhysicsEngine::checkCollision(GameObject* obj1, GameObject* obj2) {
-	return obj1->hitbox.getGlobalBounds().intersects(obj2->hitbox.getGlobalBounds());
+	if (obj1->getPosition().x > obj2->getPosition().x + obj2->getSize().x ||
+		obj1->getPosition().x + obj1->getSize().x < obj2->getPosition().x) {
+		return false;
+	}
+	if (obj1->getPosition().y > obj2->getPosition().y + obj2->getSize().y ||
+		obj1->getPosition().y + obj1->getSize().y < obj2->getPosition().y) {
+		return false;
+	}
+	return true;
 }
 
 bool PhysicsEngine::checkCollideDown(GameObject* obj1, GameObject* obj2) {
-	if (obj2->getPosition().x >= obj1->getPosition().x + obj1->getSize().x - 1 ||
-		obj2->getPosition().x + obj2->getSize().x <= obj1->getPosition().x + 1) {
+	if (obj2->getPosition().x >= obj1->getPosition().x + obj1->getSize().x - 8 ||
+		obj2->getPosition().x + obj2->getSize().x <= obj1->getPosition().x + 8) {
 		return false;
 	}
 
-	// Check if the object is below the entity
+	// Check if object2 is below object1
 	if (obj2->getPosition().y >= obj1->getPosition().y + obj1->getSize().y) {
 		return false;
 	}
@@ -61,41 +68,33 @@ bool PhysicsEngine::checkCollideDown(GameObject* obj1, GameObject* obj2) {
 }
 
 bool PhysicsEngine::checkCollideUp(GameObject* obj1, GameObject* obj2) {
-	if (obj2->getPosition().x >= obj1->getPosition().x + obj1->getSize().x - 1 ||
-		obj2->getPosition().x + obj2->getSize().x <= obj1->getPosition().x + 1) {
+	if (obj2->getPosition().x >= obj1->getPosition().x + obj1->getSize().x - 8 ||
+		obj2->getPosition().x + obj2->getSize().x <= obj1->getPosition().x + 8) {
 		return false;
 	}
 
-	// Check if the object1 is below object2 
-	if (!(obj2->getPosition().y <= obj1->getPosition().y))  {
-		return false;
-	}
-	
-	if (!checkCollision(obj1, obj2)) {
-		return false;
-	}
-	std::cout << "Collision detected" << std::endl;
-	return true;
+	// Check if the object1 is below object2 and they collide
+	return ((obj2->getPosition().y <= obj1->getPosition().y) && checkCollision(obj1, obj2));
 }
 
 bool PhysicsEngine::checkCollideLeft(GameObject* obj1, GameObject* obj2) {
-	if (obj2->getPosition().y >= obj1->getPosition().y + obj1->getSize().y - 2 ||
-		obj2->getPosition().y + obj2->getSize().y <= obj1->getPosition().y + 2) {
+	if (obj2->getPosition().y >= obj1->getPosition().y + obj1->getSize().y - 8 ||
+		obj2->getPosition().y + obj2->getSize().y <= obj1->getPosition().y + 8) {
 		return false;
 	}
 
 	// Check if the object is to the left of the entity and they collide
-	return (obj2->getPosition().x <= obj1->getPosition().x && checkCollision(obj1, obj2));
+	return (obj2->getPosition().x + 8 <= obj1->getPosition().x && checkCollision(obj1, obj2));
 }
 
 bool PhysicsEngine::checkCollideRight(GameObject* obj1, GameObject* obj2) {
-	if (obj2->getPosition().y >= obj1->getPosition().y + obj1->getSize().y - 2 ||
-		obj2->getPosition().y + obj2->getSize().y <= obj1->getPosition().y + 2) {
+	if (obj2->getPosition().y >= obj1->getPosition().y + obj1->getSize().y - 8 ||
+		obj2->getPosition().y + obj2->getSize().y <= obj1->getPosition().y + 8) {
 		return false;
 	}
 
 	// Check if the object is to the right of the entity and they collide
-	return (obj2->getPosition().x >= obj1->getPosition().x && checkCollision(obj1, obj2));	
+	return (obj2->getPosition().x >= obj1->getPosition().x + 8 && checkCollision(obj1, obj2));	
 }
 
 
@@ -104,18 +103,38 @@ void PhysicsEngine::resolveCollision(LivingEntity* entity) {
 	entity->setOnGround(false);
 
 	for (auto obj : blocks) {
+		//Resolve the right side
+
+		if (entity->isMoveRight() && checkCollideRight(entity, obj)) {
+			while (checkCollision(entity, obj)) {
+				entity->setPosition(sf::Vector2f(entity->getPosition().x - 1, entity->getPosition().y));
+			}
+			entity->setVelocity(sf::Vector2f(0, entity->getVelocity().y));
+			continue;
+		}
+
+		//Resolve the left side
+		if (entity->isMoveLeft() && checkCollideLeft(entity, obj)) {
+			while (checkCollision(entity, obj)) {
+				entity->setPosition(sf::Vector2f(entity->getPosition().x + 1, entity->getPosition().y));
+			}
+			entity->setVelocity(sf::Vector2f(0, entity->getVelocity().y));
+			continue;
+		}
+
 		//Resolve the ground
 		if (entity->getVelocity().y >= 0 && checkCollideDown(entity, obj)) {
 			entity->setOnGround(true);
 			while (checkCollision(entity,obj)) {
 				entity->setPosition(sf::Vector2f(entity->getPosition().x, entity->getPosition().y - 1));
 			}
-			entity->setPosition(sf::Vector2f(entity->getPosition().x, entity->getPosition().y + 1));
+			//entity->setPosition(sf::Vector2f(entity->getPosition().x, entity->getPosition().y + 1));
 
 			entity->setVelocity(sf::Vector2f(entity->getVelocity().x, 0));
 			entity->movementComponent->resetJumps();
 			continue;
 		}
+
 		//Resolve the ceiling
 		if (entity->getVelocity().y < 0 && checkCollideUp(entity,obj)) {
 			while (checkCollision(entity, obj)) {
@@ -125,25 +144,7 @@ void PhysicsEngine::resolveCollision(LivingEntity* entity) {
 			continue;
 		}
 
-		//Resolve the right side
-		if (entity->isMoveRight()) {
-			if (checkCollideRight(entity,obj)) {
-				while (checkCollision(entity, obj)) {
-					entity->setPosition(sf::Vector2f(entity->getPosition().x - 1, entity->getPosition().y));
-				}
-				entity->setVelocity(sf::Vector2f(0, entity->getVelocity().y));
-			}
-		}
-
-		//Resolve the left side
-		if (entity->isMoveLeft()) {
-			if (checkCollideLeft(entity, obj)) {
-				while (checkCollision(entity, obj)) {
-					entity->setPosition(sf::Vector2f(entity->getPosition().x + 1, entity->getPosition().y));
-				}
-				entity->setVelocity(sf::Vector2f(0, entity->getVelocity().y));
-			}
-		}
+		
 	}
 }
 
