f229d131e21deba92264b6cb57e0ba404d36dc8d
Dell <Dell@DESKTOP-PRIISU0>
2024-11-29 18:29:57 +0700

    Added CoinBlock

--- Source/CoinBlock.cpp
+++ Source/CoinBlock.cpp
@@ -1,1 +1,32 @@
 #include "Headers/CoinBlock.h"
+
+CoinBlock::CoinBlock()
+{
+	this->numCoins = 0;
+}
+
+CoinBlock::CoinBlock(sf::Vector2f position, sf::Vector2f size, std::string name, int numCoins) :
+	SolidBlock(position, size, name)
+{
+	this->numCoins = numCoins;
+}
+
+CoinBlock::~CoinBlock()
+{
+}
+
+void CoinBlock::update(const float& dt)
+{
+}
+
+void CoinBlock::reactToObject(int collidedSide)
+{
+}
+
+void CoinBlock::render(sf::RenderTarget* target)
+{
+	target->draw(entitySprite);
+	target->draw(hitbox);
+}
+
+
--- Source/Headers/CoinBlock.h
+++ Source/Headers/CoinBlock.h
@@ -3,5 +3,18 @@
 class CoinBlock :
     public SolidBlock
 {
+private:
+	int numCoins;
+
+public:
+	CoinBlock();
+	CoinBlock(sf::Vector2f position, sf::Vector2f size, std::string name, int numCoins = 3);
+	virtual ~CoinBlock();
+	//void initSpritesSheet();
+
+	//Setters and Getters
+	void update(const float& dt) override;
+	void reactToObject(int collidedSide) override;
+	void render(sf::RenderTarget* target) override;
 };
 
