647ba840da22089eac2fe0f8d764fc16130cd186
Dang Vo <146331706+dariomv28@users.noreply.github.com>
2024-12-14 12:19:18 +0700

    nothing

--- Source/AudioSystem.cpp
+++ Source/AudioSystem.cpp
@@ -2,7 +2,7 @@
 
 
 AudioSystem::AudioSystem() {
-	if (!music.openFromFile("Source/Resources/audio/Background Music.mp3")) {
+	if (!music.openFromFile("Source/Resources/audio/BackGround Music.mp3")) {
 		throw("ERROR::AUDIOSYSTEM::CANNOT_LOAD_BACKGROUND_MUSIC");
 	}
 	if (!jumpSoundBuffer.loadFromFile("Source/Resources/audio/Jump.mp3")) {
@@ -11,6 +11,15 @@ AudioSystem::AudioSystem() {
 	//if (!buttonSoundBuffer.loadFromFile("Source/Resources/audio/Button.mp3")) {
 		//throw("ERROR::AUDIOSYSTEM::CANNOT_LOAD_JUMP_SOUND_EFFECT");
 	//}
+	if (!level1Music.openFromFile("Source/Resources/audio/Level 1.mp3")) {
+		throw("ERROR::AUDIOSYSTEM::CANNOT_LOAD_LEVEL_1_MUSIC");
+	}
+	if (!level2Music.openFromFile("Source/Resources/audio/Level 2.mp3")) {
+		throw("ERROR::AUDIOSYSTEM::CANNOT_LOAD_LEVEL_2_MUSIC");
+	}
+	if (!level3Music.openFromFile("Source/Resources/audio/Level 3.mp3")) {
+		throw("ERROR::AUDIOSYSTEM::CANNOT_LOAD_LEVEL_3_MUSIC");
+	}
 	jumpSound.setBuffer(jumpSoundBuffer);
 	buttonSound.setBuffer(buttonSoundBuffer);
 }
@@ -20,10 +29,49 @@ void AudioSystem::playMusic() {
 	music.play();
 }
 
+void AudioSystem::stopMusic() {
+	music.stop();
+}
+
+void AudioSystem::playLevel1Music() {
+	level1Music.setLoop(true);
+	level1Music.play();
+}
+
+void AudioSystem::stopLevel1Music() {
+	level1Music.stop();
+}
+
+void AudioSystem::playLevel2Music() {
+	level2Music.setLoop(true);
+	level2Music.play();
+}
+
+void AudioSystem::stopLevel2Music() {
+	level2Music.stop();
+}
+
+void AudioSystem::playLevel3Music() {
+	level3Music.setLoop(true);
+	level3Music.play();
+}
+
+void AudioSystem::stopLevel3Music() {
+	level3Music.stop();
+}
+
 void AudioSystem::playJumpSound() {
 	jumpSound.play();
 }
 
+void AudioSystem::stopJumpSound() {
+
+}
+
 void AudioSystem::playbuttonSound() {
 	buttonSound.play();
+}
+
+void AudioSystem::stopButtonSound() {
+
 }
--- Source/Headers/AudioSystem.h
+++ Source/Headers/AudioSystem.h
@@ -4,6 +4,9 @@ class AudioSystem
 {
 private:
 	sf::Music music;
+	sf::Music level1Music;
+	sf::Music level2Music;
+	sf::Music level3Music;
 	sf::Sound jumpSound;
 	sf::Sound buttonSound;
 	sf::SoundBuffer jumpSoundBuffer;
@@ -11,7 +14,16 @@ private:
 public:
 	AudioSystem();
 	void playMusic();
+	void stopMusic();
+	void playLevel1Music();
+	void stopLevel1Music();
+	void playLevel2Music();
+	void stopLevel2Music();
+	void playLevel3Music();
+	void stopLevel3Music();
 	void playJumpSound();
+	void stopJumpSound();
 	void playbuttonSound();
+	void stopButtonSound();
 };
 
