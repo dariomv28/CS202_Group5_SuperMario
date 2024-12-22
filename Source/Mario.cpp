7de101ed308548a63f1ac821201ab68bf9bd2233
KietAPCS <phantuankiet688@gmail.com>
2024-12-22 12:29:25 +0700

    NEED FIXING VIEW BUG IN DEATH MENU

--- Source/Mario.cpp
+++ Source/Mario.cpp
@@ -296,14 +296,12 @@ void Mario::handleInput(const float& dt) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
         movementComponent->isMoveLeft = true;
         entitySprite.setScale(4.0f, 4.0f);
-        //hitbox.setScale(1.0f, 1.0f);
         isMovingLeft = true;
         isWalking = true;
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
         movementComponent->isMoveRight = true;
         entitySprite.setScale(4.0f, 4.0f);
-        //hitbox.setScale(1.0f, 1.0f);
         isWalking = true;
         isMovingLeft = false;
     }
