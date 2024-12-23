243b6b0fe2840313378ff638cf44c985ae955a07
KietAPCS <phantuankiet688@gmail.com>
2024-12-23 13:24:03 +0700

    PUSH ENEMIES FOR W3_LV2 - FINISH ENEMIES

--- Source/W3_LV3.cpp
+++ Source/W3_LV3.cpp
@@ -4,6 +4,7 @@ W3_LV3::W3_LV3(PlayerManager* player, sf::RenderWindow* window):
 	LevelManager(player, window) 
 {
 	mapManager->convert_sketch(3,3, Enemies, Blocks, PowerUps, player);
+
 	initGameEventMediator();
 }
 
